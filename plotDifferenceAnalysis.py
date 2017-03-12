d684a861c820070f6a25569ecf3dc6fd8e06a050
Lanqing Huang <lqhuang@outlook.com>
2017-03-13 02:30:45 +0800

    Update DifferenceAnalysis plot function

--- plotDifferenceAnalysis.py
+++ plotDifferenceAnalysis.py
@@ -4,7 +4,8 @@ from saxsio import dat
 from DifferenceAnalysis import DifferenceAnalysis
 from matplotlib.pyplot import close
 
-def plot_DifferenceAnalysis(root_directory, save_figures=False, figures_directory=None):
+def plot_DifferenceAnalysis(root_directory, display=False, dash_line_index=None,
+                            save_figures=False, figures_directory=None):
 
     file_location = os.path.join(root_directory, 'Simple_Results')
     scat_obj = DifferenceAnalysis.from_subtracted_dats(os.path.join(file_location, '*'))
@@ -12,10 +13,10 @@ def plot_DifferenceAnalysis(root_directory, save_figures=False, figures_director
     if not figures_directory:
         figures_directory = os.path.join(root_directory, 'Figures')
     EXP_prefix = os.path.basename(root_directory)
-    scat_obj.plot_relative_diff(display=False,
+    scat_obj.plot_relative_diff(display=display, dash_line_index=dash_line_index,
                                 save=save_figures, filename=EXP_prefix+'_relative_ratio.png',
                                 directory=figures_directory)
-    scat_obj.plot_profiles(log_intensity=True, display=False,
+    scat_obj.plot_profiles(log_intensity=True, display=False, dash_line_index=dash_line_index,
                            save=save_figures, filename=EXP_prefix+'_saxs_profiles.png',
                            directory=figures_directory)
 
@@ -27,7 +28,19 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--figures_directory',
                         help='Figures directory in root directory for Difference Analysis (default=Figures)',
                         default='Figures')
+    parser.add_argument('--display', type=bool)
+    parser.add_argument('--dash_line_index', help='Index starts with 1', type=str, default=None)
     args = parser.parse_args()
+    print(args)
     root_directory = args.root_directory
     figures_directory = os.path.join(root_directory, args.figures_directory)
-    plot_DifferenceAnalysis(root_directory, save_figures=True, figures_directory=figures_directory)
+    try:
+        dash_line_index = args.dash_line_index.split(',')
+        dash_line_index = [int(idx) for idx in dash_line_index]
+        print('dash line index:', dash_line_index)
+    except AttributeError:
+        dash_line_index = [-1, -1]
+    display = args.display
+    plot_DifferenceAnalysis(root_directory, dash_line_index=dash_line_index,
+                            display=display,
+                            save_figures=True,figures_directory=figures_directory)
