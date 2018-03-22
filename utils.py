1a71759d4cabfdbeccde4b9c3c584e7e14a89095
Lanqing Huang <lqhuang@outlook.com>
2018-03-22 14:36:24 +0800

    Remove irrelevant codes

--- utils.py
+++ utils.py
@@ -33,10 +33,10 @@ def find_common_string_from_list(string_list):
     return common_string
 
 
-def find_common_string(str_a, str_b):
-    match = SequenceMatcher(None, str_a, str_b).find_longest_match(
-        0, len(str_a), 0, len(str_b))
-    common_string = str_a[match.str_a:match.size]
+def find_common_string(a, b):
+    match = SequenceMatcher(None, a, b).find_longest_match(
+        0, len(a), 0, len(b))
+    common_string = a[match.a:match.size]
     return common_string
 
 
